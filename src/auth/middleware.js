b6d800bf137461f8f8f308bd75e15657d7eddb3b
Chance Harmon <henrikillsit@gmail.com>
2019-08-26 20:11:07 -0700

    a few bugs in MW

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -5,44 +5,39 @@ const User = require('./users-model.js');
 module.exports = (req, res, next) => {
 
   try {
-
-    let [authType, encodedString] = req.headers.authorization.split(/\s+/);
-
-    // BASIC Auth  ... Authorization:Basic ZnJlZDpzYW1wbGU=
-
-    switch(authType.toLowerCase()) {
-      case 'basic':
-        return _authBasic(encodedString);
-      default:
+    try {
+      let [type, authString] = request.headers.authorization.split(' ');
+      if (type === 'basic') {
+        return _authBasic(authString);
+      } else {
         return _authError();
+      }
+
+    } catch (error) {
+      return _authError();
     }
 
-  } catch(e) {
-    return _authError();
-  }
-
-  function _authBasic() {
-    let base64Buffer = Buffer.from(authString,'base64'); // <Buffer 01 02...>
-    let bufferString = base64Buffer.toString(); // john:mysecret
-    let [username,password] = bufferString.split(':');  // variables username="john" and password="mysecret"
-    let auth = [username,password];  // {username:"john", password:"mysecret"}
-
-    return User.authenticateBasic(auth)
-      .then( user => _authenticate(user) );
-  }
-
-  function _authenticate(user) {
-    if ( user ) {
-      next();
+    function _authBasic() {
+      const decodedString = Buffer.from(authString, 'base64').toString();
+      const [username, password] = decodedString.split(':');
+      const userSignIn = { username, password };
+      return User.authenticateBasic(userSignIn)
+        .then(user => _authenticate(user));
     }
-    else {
-      _authError();
+
+    function _authenticate(user) {
+      if (user) {
+        request.user = user;
+        request.token = user.generateToken();
+        next();
+      } else {
+        return _authError();
+      }
     }
-  }
 
-  function _authError() {
-    next({status: 401, statusMessage: 'Unauthorized', message: 'Invalid User ID/Password'});
-  }
+    function _authError() {
+      next({ status: 401, statusMessage: 'Unauthorized', message: 'Invalid User ID/Password' });
+    }
 
-};
+  };
 
